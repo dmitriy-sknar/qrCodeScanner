49ed73882d30567321e3ef2ccc9b5b41dcf4b352
dmitriy sknar <dmitriy.sknar@g,mail.com>
2016-09-08 16:26:55 +0300

    Added check for camera presence on device.

--- app/src/main/java/com/ioLab/qrCodeScanner/SplashScreen.java
+++ app/src/main/java/com/ioLab/qrCodeScanner/SplashScreen.java
@@ -1,6 +1,10 @@
 package com.ioLab.qrCodeScanner;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -15,16 +19,39 @@ public class SplashScreen extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
-        new Thread() {
-            public void run() {
-                try {
-                    TimeUnit.SECONDS.sleep(2);
-                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-                    startActivity(intent);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        //to consider if to continue work if camera is missing on the device
+        if(isCameraAvailable()) {
+            new Thread() {
+                public void run() {
+                    try {
+                        TimeUnit.SECONDS.sleep(2);
+                        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                        startActivity(intent);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
+            }.start();
+        }
+        else{
+            showDialog(this);
+        }
+    }
+
+    public boolean isCameraAvailable() {
+        PackageManager pm = getPackageManager();
+        return pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+    private void showDialog(final Activity act) {
+        AlertDialog.Builder downloadDialog = new AlertDialog.Builder(act);
+        downloadDialog.setTitle(R.string.splash_screen_alert_dialog);
+        downloadDialog.setMessage(R.string.splash_screen_alert_dialog_message);
+        downloadDialog.setPositiveButton(R.string.ok_button, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
             }
-        }.start();
+        });
+        downloadDialog.show();
     }
 }
